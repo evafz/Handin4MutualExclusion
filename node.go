3df7e8f98c65165532a557e42dc2692b438efb7a
Eva <evaf@itu.dk>
2023-11-12 17:40:34 +0100

    Update node.go
    not tried/tested

--- node.go
+++ node.go
@@ -1,12 +1,68 @@
 package handin4mutualexclusion
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Node struct {
-	id                int
-	port              int
-	inCriticalSection bool
+	id       int
+	port     int
+	hasToken bool
+	Mutex    sync.Mutex
 }
 
-func EnterRequest (Id int) {
+func main() {
+	//make three nodes in an array
+	nodeIDs := []int{1, 2, 3}
+	nodes := make([]*Node, len(nodeIDs))
+	//set the id of the nodes to the id
+	for i, id := range nodeIDs {
+		nodes[i] = &Node{
+			id: id,
+		}
+	}
+
+	//start with one node having the token:
+	startWithToken := nodes[0]
+	startWithToken.hasToken = true
+
+	//start up the nodes
+
+	for i := 0; i < 3; i++ {
+		go func(node *Node) {
+			node.enterCriticalSection(nodes)
+		}(nodes[i%len(nodes)])
+	}
+}
+
+func (node *Node) enterCriticalSection(nodes []*Node) {
+	node.Mutex.Lock()
+
+	fmt.Println("A node is now in the critical section")
+	//Sleep
+	fmt.Println("The node has now left the critical section")
+
+	nextNodeID := (node.id + 1) % len(nodes)
+	node.passToken(nextNodeID, nodes)
+
+}
+
+func (node *Node) passToken(nextID int, nodes []*Node) {
+	nextNode := nodes[nextID]
+	nextNode.Mutex.Lock()
+
+	node.hasToken = false
+	node.Mutex.Unlock()
+
+	nextNode.hasToken = true
+
+	nextNode.Mutex.Unlock()
+
+}
+
+/*
+func EnterRequest(Id int) {
 	//wait
 	//ask if others are in critsect
 	//if one is true, wait and ask again over and over until they're not
@@ -14,7 +70,8 @@ func EnterRequest (Id int) {
 	//call exit once done
 }
 
-func Exit (Id int) {
+func Exit(Id int) {
 	//set inCriticalSection to false
 	//start over with EnterRequest
 }
+*/
